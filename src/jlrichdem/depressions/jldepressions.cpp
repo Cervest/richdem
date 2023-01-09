a54faea94a9c57427182267914104381ebaaf91a
Sunayana Ghosh <sunayana@cervest.earth>
2023-01-09 10:18:25 +0100

    trying to map a c++ struct to a corresponding julia struct

--- src/jlrichdem/depressions/jldepressions.cpp
+++ src/jlrichdem/depressions/jldepressions.cpp
@@ -8,23 +8,43 @@
 #include <richdem/depressions/fill_spill_merge.hpp>
 namespace rd = richdem;
 
-JLCXX_MODULE define_depressions_module(jlcxx::Module &mod)
+// JLCXX_MODULE define_depressions_module(jlcxx::Module &mod)
+// {
+//     jlcxx::TypeWrapper<rd::dephier::Depression<double>> depression_double =
+//         mod.add_type<rd::dephier::Depression<double>>("DepressionDouble");
+
+//     jlcxx::TypeWrapper<rd::dephier::DepressionHierarchy<double>> depression_hierarchy_double =
+//         mod.add_type<rd::dephier::DepressionHierarchy<double>>("DepressionHierarchyDouble");
+
+//     mod.method("GetDepressionHierarchyDoubleD8", [](const rd::Array2D<double> &topo,
+//                                                     rd::Array2D<rd::dephier::dh_label_t> &label,
+//                                                     rd::Array2D<int8_t> &flowdirs)
+//                { return rd::dephier::GetDepressionHierarchy<double, rd::Topology::D8>(topo, label, flowdirs); });
+
+//     mod.method("FillSpillMerge", [](const rd::Array2D<double> &topo,
+//                                     const rd::Array2D<rd::dephier::dh_label_t> &label,
+//                                     const rd::Array2D<rd::flowdir_t> &flowdirs,
+//                                     rd::dephier::DepressionHierarchy<double> &deps,
+//                                     rd::Array2D<double> &wtd)
+//                { return rd::dephier::FillSpillMerge(topo, label, flowdirs, deps, wtd); });
+// }
+
+namespace jlrichdem
 {
-    jlcxx::TypeWrapper<rd::dephier::Depression<double>> depression_double =
-        mod.add_type<rd::dephier::Depression<double>>("DepressionDouble");
+    struct WrapDepression
+    {
+        template <typename TypeWrapperT>
+        void operator()(TypeWrapperT &&wrapped)
+        {
+            using WrappedT = typename TypeWrapperT::type;
+        }
+    };
+}
 
-    jlcxx::TypeWrapper<rd::dephier::DepressionHierarchy<double>> depression_hierarchy_double =
-        mod.add_type<rd::dephier::DepressionHierarchy<double>>("DepressionHierarchyDouble");
-
-    mod.method("GetDepressionHierarchyDoubleD8", [](const rd::Array2D<double> &topo,
-                                                    rd::Array2D<rd::dephier::dh_label_t> &label,
-                                                    rd::Array2D<int8_t> &flowdirs)
-               { return rd::dephier::GetDepressionHierarchy<double, rd::Topology::D8>(topo, label, flowdirs); });
-
-    mod.method("FillSpillMerge", [](const rd::Array2D<double> &topo,
-                                    const rd::Array2D<rd::dephier::dh_label_t> &label,
-                                    const rd::Array2D<rd::flowdir_t> &flowdirs,
-                                    rd::dephier::DepressionHierarchy<double> &deps,
-                                    rd::Array2D<double> &wtd)
-               { return rd::dephier::FillSpillMerge(topo, label, flowdirs, deps, wtd); });
+JLCXX_MODULE define_depressions_module(jlcxx::Module &types)
+{
+    using jlcxx::Parametric;
+    using jlcxx::TypeVar;
+    types.add_type<Parametric<TypeVar<1>>>("Depression", jlcxx::julia_type("Depression"))
+        .apply<rd::dephier::Depression<double>>(jlrichdem::WrapDepression());
 }
