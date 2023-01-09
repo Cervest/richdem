6103b7b07ac47c7c25dee8a92615d06dc4113af8
Sunayana Ghosh <sunayana@cervest.earth>
2023-01-09 15:43:51 +0100

    Wrapping Depression template struct using map_type.

--- src/jlrichdem/depressions/jldepressions.cpp
+++ src/jlrichdem/depressions/jldepressions.cpp
@@ -8,13 +8,8 @@
 #include <richdem/depressions/fill_spill_merge.hpp>
 namespace rd = richdem;
 
-struct TestDepression
-{
-    rd::dephier::flat_c_idx pit_cell = rd::dephier::NO_VALUE;
-    float pit_elev = std::numeric_limits<float>::infinity();
-};
-
 JLCXX_MODULE define_depressions_module(jlcxx::Module &mod)
 {
-    mod.add_type<TestDepression>("TestDepression", jlcxx::julia_type("TestDepression"));
+    mod.map_type<rd::dephier::Depression<float>>("Depression");
+    mod.map_type<rd::dephier::Depression<double>>("Depression");
 }
--- wrappers/RichDEM.jl/src/RichDEM.jl
+++ wrappers/RichDEM.jl/src/RichDEM.jl
@@ -25,11 +25,24 @@ end #misc
 
 module depressions
     using CxxWrap
-    mutable struct TestDepression
+    mutable struct Depression{T <: Real}
         pit_cell::UInt32
-        pit_elev::Float32
+        out_cell::UInt32
+        parent::UInt32
+        odep::UInt32
+        geolink::UInt32
+        pit_elev::T
+        out_elev::T
+        lchild::UInt32
+        rchild::UInt32
+        ocean_parent::Bool
+        ocean_linked::Vector{UInt32}
+        dep_label::UInt32
+        cell_count::UInt32
+        dep_vol::Float64
+        water_vol::Float64
+        total_elevation::Float64
     end
-    export TestDepression
     @wrapmodule("/workspaces/richdem/build/lib/libjlrichdem.so", :define_depressions_module)
 
     function __init__()
