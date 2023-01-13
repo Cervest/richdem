568f8a81a26fed16b541df45e0d35e3cce3ee1be
Sunayana Ghosh <sunayana@cervest.earth>
2023-01-13 14:51:23 +0100

    just testing

--- wrappers/RichDEM.jl/src/RichDEM.jl
+++ wrappers/RichDEM.jl/src/RichDEM.jl
@@ -25,30 +25,30 @@ end #misc
 
 module depressions
     using CxxWrap
-    mutable struct DepressionAllocated{T}
-        pit_cell::UInt32
-        out_cell::UInt32
-        parent::UInt32
-        odep::UInt32
-        geolink::UInt32
-        pit_elev::T
-        out_elev::T
-        lchild::UInt32
-        rchild::UInt32
-        ocean_parent::Bool
-        ocean_linked::Vector{UInt32}
-        dep_label::UInt32
-        cell_count::UInt32
-        dep_vol::Float64
-        water_vol::Float64
-        total_elevation::Float64
-    end
+    # struct DepressionAllocated{T}
+    #     pit_cell::UInt32
+    #     out_cell::UInt32
+    #     parent::UInt32
+    #     odep::UInt32
+    #     geolink::UInt32
+    #     pit_elev::T
+    #     out_elev::T
+    #     lchild::UInt32
+    #     rchild::UInt32
+    #     ocean_parent::Bool
+    #     ocean_linked::Vector{UInt32}
+    #     dep_label::UInt32
+    #     cell_count::UInt32
+    #     dep_vol::Float64
+    #     water_vol::Float64
+    #     total_elevation::Float64
+    # end
     @wrapmodule("/workspaces/richdem/build/lib/libjlrichdem.so", :define_depressions_module)
 
     function __init__()
         @initcxx
     end
-
+    
     # Base.size(dep_hier::DepressionHierarchyDouble) = size(dep_hier)
     # Base.IndexStyle(::Type{<:DepressionHierarchyDouble}) = IndexCartesian()
 end #depressions
